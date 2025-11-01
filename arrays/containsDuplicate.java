7d09af18219f1a717caacc0c0b24bf38fcfc55b3
Yug Kapri <166427458+Yug-Kapri@users.noreply.github.com>
2025-11-02 00:30:51 +0530

    Add containsDuplicate method to check for duplicates
    Implement a method to check for duplicates in an array.

---
+++ arrays/containsDuplicate.java
@@ -0,0 +1,17 @@
+class Soluton {
+  public boolean containsDuplicate(int[] nums) {
+    // Create a HasSet to store elements from the array
+    HashSet<Integer> number = new HashSet<>();
+
+    // Iterate through each element in the array
+    for(int num : nums) {
+      // Check if the element is already in the HashSet
+      if(number.contains(num)) {
+        return true; // Duplicate found
+      }
+      // Add the element to the HashSet
+      number.add(num);
+    }
+    return false; //No duplicate found
+  }
+}  
